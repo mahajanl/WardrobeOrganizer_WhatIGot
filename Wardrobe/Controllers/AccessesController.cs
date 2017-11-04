319aeb304237305043c082cfc44c1a6af617306b
mahajanl <lakshamima@gmail.com>
2017-11-04 13:54:02 -0400

    primary commit

---
+++ Wardrobe/Controllers/AccessesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wardrobe.Models;
+
+namespace Wardrobe.Controllers
+{
+    public class AccessesController : Controller
+    {
+        private WardrobeEntities db = new WardrobeEntities();
+
+        // GET: Accesses
+        public ActionResult Index()
+        {
+            var accesses = db.Accesses.Include(a => a.Color).Include(a => a.Occasion).Include(a => a.Season);
+            return View(accesses.ToList());
+        }
+
+        // GET: Accesses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Access access = db.Accesses.Find(id);
+            if (access == null)
+            {
+                return HttpNotFound();
+            }
+            return View(access);
+        }
+
+        // GET: Accesses/Create
+        public ActionResult Create()
+        {
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName");
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "OccasionType");
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "TheSeason");
+            return View();
+        }
+
+        // POST: Accesses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccessID,Name,Photo,AccessType,ColorID,SeasonID,OccasionID")] Access access)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accesses.Add(access);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName", access.ColorID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "OccasionType", access.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "TheSeason", access.SeasonID);
+            return View(access);
+        }
+
+        // GET: Accesses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Access access = db.Accesses.Find(id);
+            if (access == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName", access.ColorID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "OccasionType", access.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "TheSeason", access.SeasonID);
+            return View(access);
+        }
+
+        // POST: Accesses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccessID,Name,Photo,AccessType,ColorID,SeasonID,OccasionID")] Access access)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(access).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ColorID = new SelectList(db.Colors, "ColorID", "ColorName", access.ColorID);
+            ViewBag.OccasionID = new SelectList(db.Occasions, "OccasionID", "OccasionType", access.OccasionID);
+            ViewBag.SeasonID = new SelectList(db.Seasons, "SeasonID", "TheSeason", access.SeasonID);
+            return View(access);
+        }
+
+        // GET: Accesses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Access access = db.Accesses.Find(id);
+            if (access == null)
+            {
+                return HttpNotFound();
+            }
+            return View(access);
+        }
+
+        // POST: Accesses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Access access = db.Accesses.Find(id);
+            db.Accesses.Remove(access);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
